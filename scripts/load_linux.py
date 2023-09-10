91b106fd4f15f895b409b0855d2980650fae69bb
Ripley White <pearlcodes@outlook.com>
2023-09-10 19:01:31 +0100

    nicer prints, added kernel driver attach/detach.

--- scripts/load_linux.py
+++ scripts/load_linux.py
@@ -11,6 +11,8 @@ parser.add_argument('-k', '--kernel', dest='kernel', help='path to kernel image'
 parser.add_argument('-d', '--dtbpack', dest='dtbpack', help='path to dtbpack')
 parser.add_argument('-r', '--initrd', dest='initrd', help='path to initial ramdisk')
 parser.add_argument('-c', '--cmdline', dest='cmdline', help='custom kernel command line')
+parser.add_argument('-pid', '--usb-product-id', dest='usb_pid', help='usb product id if device isnt detected. find it with gaster.')
+parser.add_argument('-vid', '--usb-vendor-id', dest='usb_vid', help='usb vendor id if device isnt detected. find it with gaster.')
 
 args = parser.parse_args()
 
@@ -22,12 +24,18 @@ if args.dtbpack is None:
     print(f"error: No dtbpack specified! Run `{sys.argv[0]} --help` for usage.")
     exit(1)
 
-dev = usb.core.find(idVendor=0x05AC, idProduct=0x1227)
+if args.usb_pid is None:
+    args.usb_pid = 0x1227
+
+if args.usb_vid is None:
+    args.usb_vid = 0x05AC
+
+dev = usb.core.find(idVendor=args.usb_vid, idProduct=args.usb_pid)
 if dev is None:
-    print("Waiting for device...")
+    print("[*] finding device.. replug if you're in dfu and device isn't detected.")
 
     while dev is None:
-        dev = usb.core.find(idVendor=0x05AC, idProduct=0x1227)
+        dev = usb.core.find(idVendor=args.usb_vid, idProduct=args.usb_pid)
         if dev is not None:
             dev.set_configuration()
             break
@@ -38,12 +46,15 @@ else:
 kernel = open(args.kernel, "rb").read()
 fdt = open(args.dtbpack, "rb").read()
 
+if not dev.is_kernel_driver_active(0):
+    dev.detach_kernel_driver(0)
+
 if args.cmdline is not None:
     dev.ctrl_transfer(0x21, 4, 0, 0, 0)
     dev.ctrl_transfer(0x21, 3, 0, 0, f"linux_cmdline {args.cmdline}\n")
 
 if args.initrd is not None:
-    print("Loading initial ramdisk...")
+    print("[*] loading initrd...")
     initrd = open(args.initrd, "rb").read()
     initrd_size = len(initrd)
     dev.ctrl_transfer(0x21, 2, 0, 0, 0)
@@ -52,30 +63,32 @@ if args.initrd is not None:
     dev.write(2, initrd, 1000000)
     dev.ctrl_transfer(0x21, 4, 0, 0, 0)
     dev.ctrl_transfer(0x21, 3, 0, 0, "ramdisk\n")
-    print("Initial ramdisk loaded successfully.")
+    print("[*] loaded initrd")
 
-print("Loading device tree...")
+print("[*] loading device tree")
 dev.ctrl_transfer(0x21, 2, 0, 0, 0)
 dev.ctrl_transfer(0x21, 1, 0, 0, 0)
 dev.write(2, fdt)
 
 dev.ctrl_transfer(0x21, 4, 0, 0, 0)
 dev.ctrl_transfer(0x21, 3, 0, 0, "fdt\n")
-print("Device tree loaded successfully.")
+print("[*] loaded device tree")
 
-print("Loading kernel...")
+print("[*] loading kernel")
 kernel_size = len(kernel)
 dev.ctrl_transfer(0x21, 2, 0, 0, 0)
 dev.ctrl_transfer(0x21, 1, 0, 0, struct.pack('I', kernel_size))
 
 dev.write(2, kernel, 1000000)
-print("Kernel loaded successfully.")
+print("[*] loaded kernel")
 
 dev.ctrl_transfer(0x21, 4, 0, 0, 0)
 
-print("Booting...")
+print("[*] booting device")
 try:
     dev.ctrl_transfer(0x21, 3, 0, 0, "bootl\n")
 except:
     # if the device disconnects without acknowledging it usually means it succeeded
-    print("Success.")
+    print("[*] device should be booting (disconnected without acknowledge). if not, pray to god and they'll tell you why.")
+
+dev.attach_kernel_driver(0)
